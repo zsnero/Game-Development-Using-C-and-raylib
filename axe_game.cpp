1835736658eaa8d78e062dec3b6e231cc84bd3c4
Snero <sarathkr@yandex.com>
2022-04-16 00:02:37 +0530

    Add files via upload

---
+++ axe_game.cpp
@@ -0,0 +1,47 @@
+#include "raylib.h"
+int main()
+{
+    // window dimensions
+    int width = 350, height = 200;
+    InitWindow(width, height, "My Window");
+
+    // circle coordinates
+    int circle_x = 175;
+    int circle_y = 100;
+
+    SetTargetFPS(60);
+    while (WindowShouldClose()==false)
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Game logic begins
+        DrawCircle(circle_x, circle_y, 25, RED);
+
+        if (IsKeyDown(KEY_D) && circle_x < 350)
+        {
+            circle_x = circle_x + 10;
+        }
+
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x = circle_x - 10;
+        }
+
+        if (IsKeyDown(KEY_W) && circle_y > 0)
+        {
+            circle_y = circle_y - 10;
+        }
+
+        if (IsKeyDown(KEY_S) && circle_y < 200)
+        {
+            circle_y = circle_y + 10;
+        }
+
+
+
+        // Game logic ends
+        EndDrawing();
+
+    }
+}
